cc046156e1465240f156361d6f7924f248aafe7e
Kalimullin Arthur <ironstone3105@gmail.com>
2023-06-18 09:50:15 +0500

    external images

--- src/code/ImageLoader.cs
+++ src/code/ImageLoader.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 static class ImageLoader
 {
     static readonly string MISSING_TEXTURE_PATH = "res://content/missing.png";
@@ -7,7 +9,7 @@ static class ImageLoader
         Texture2D texture;
 
         try  {
-            texture = GD.Load<Texture2D>(path);
+            texture = GetTextureFromPath(path);
         }
         catch(Exception ex) {
             GD.PrintErr($"Failed to load image {path}. Exception: {ex.Message}");
@@ -15,5 +17,15 @@ static class ImageLoader
         }
 
         return texture;
+    }
+
+    static Texture2D GetTextureFromPath(string path) 
+    {
+        if(path.StartsWith("res")) {
+            return GD.Load<Texture2D>(path);
+        }
+
+        var image = Image.LoadFromFile(path);
+        return ImageTexture.CreateFromImage(image);
     }
 }
